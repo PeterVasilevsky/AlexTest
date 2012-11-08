6bdd9d6f54e32fe061350b44353f3cbb01845142
PeterVasilevsky <zeelony@me.com>
2012-11-08 23:08:41 +0400

    Initial Commit

---
+++ AlexTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  AlexTest
+//
+//  Created by Петр Василевский on 08.11.12.
+//  Copyright (c) 2012 Петр Василевский. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
